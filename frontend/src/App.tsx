c722c1c87729b7ef9c78df494da0fdefc939f547
devGiovanni95 <giovanni.DevJava@hotmail.com>
2021-09-07 12:40:56 -0300

    Criando o componente Footer-Rodapé

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,16 @@
+import Footer from "conponents/Footer";
+import NavBar from "conponents/NavBar";
 
 function App() {
   return (
+//serve pra fragmentar o componente 
+<>
+    <NavBar/>
     <div>
-      <h1 className="text-primary">Olá Mundo!</h1>
+    <h1 className="text-primary">Olá Mundo!</h1>
     </div>
+    <Footer/>
+    </>
   );
 }
 
