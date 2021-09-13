0476f494666703c06f41b4a225e6a13a11955e27
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-09-12 23:30:18 -0300

    arChart Integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,67 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+
+type SeriesData = {
+    name: string;
+    data: number[];
+}
+
+type ChartData = {
+    labels: {
+        categories: string[];
+    };
+    series: SeriesData[];
+}
+
 
 const BarChart = () => {
+
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: {
+            categories: []
+        },
+        series: [
+            {
+                name: "",
+                data: []
+            }
+        ]
+    });
+
+
+    //Forma Correta
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/sales/success-by-seller`)
+            .then((response) => {
+
+                const data = response.data as SaleSuccess[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => round(100.0 * x.deals / x.visited, 1));//quantidade de casa que quer ver
+
+                setChartData({
+
+                    labels: {
+                        categories: myLabels//estava errado com []
+                    },
+                    series: [
+                        {
+                            name: "% Success",
+                            data: mySeries
+                        }
+                    ]
+                });
+
+            });
+    },
+        []
+    )
+
 
     const options = {
         plotOptions: {
@@ -10,28 +71,28 @@ const BarChart = () => {
         },
     };
 
-    const mockData = {
-        labels: {
-            categories: ['Giovanni', 'Fernando', 'Barry Allen','Logan','Maria']
-        },
-        series: [
-            {
-                name: "% Sucesso",
-                data: [90.0,67.1,47.9,79.0,85.4]
-            }
-        ]
-    };
+    //   const mockData = {
+    //     labels: {
+    //       categories: ['Giovanni', 'Fernando', 'Barry Allen', 'Logan', 'Maria']
+    //  },
+    // series: [
+    //    {
+    //      name: "% Sucesso",
+    //    data: [90.0, 67.1, 47.9, 79.0, 85.4]
+    // }
+    //       ]
+    // };
 
 
-  return(
-    <Chart 
-        //options={options}//definir a funcao 
-        options={{...options, xaxis: mockData.labels}}//dados do eixo x / Rotulos
-        series={mockData.series}//valores
-        type="bar"//tipo de grafico barras
-        height="240"//altura do grafico
-    />
-);
+    return (
+        <Chart
+            //options={options}//definir a funcao 
+            options={{ ...options, xaxis: chartData.labels }}//dados do eixo x / Rotulos
+            series={chartData.series}//valores
+            type="bar"//tipo de grafico barras
+            height="240"//altura do grafico
+        />
+    );
 }
 
 export default BarChart;
--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
-import { SAleSum } from 'types/sale';
+import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
@@ -18,7 +18,7 @@ const DonutChar = () => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then((response) => {
 
-            const data = response.data as SAleSum[];
+            const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
