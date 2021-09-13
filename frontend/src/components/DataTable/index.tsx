94d895262ea1b5fd47a4c6b3edfdae437f8b49be
devGiovanni95 <giovanni.devjava@hotmail.com>
2021-09-13 00:46:51 -0300

    Create functios for paginatios previus and next

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import Pagination from "components/Pagination";
 import { useEffect, useState } from "react";
 import { SalePage } from "types/sale";
 import { formatLocalDate } from "utils/format";
 import { BASE_URL } from "utils/requests";
 
 const DataTable = () => {
+
+    const [activePage, setActivePage] = useState(0);
 
     const [page, setPage] = useState<SalePage>({
         first: true,
@@ -22,7 +25,13 @@ const DataTable = () => {
 
     }, []);
 
+    const changePage = (index: number) => {
+        setActivePage(index);
+    }
+
   return(
+      <>
+      <Pagination page={page} onPageChange={changePage}/>
   
     <div className="table-responsive">
         <table className= "table table-striped table-sm">
@@ -56,7 +65,7 @@ const DataTable = () => {
             </tbody>
         </table>
     </div>
-
+</>
 );
 }
 
